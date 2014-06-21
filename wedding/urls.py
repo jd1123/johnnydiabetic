b65a988373ae5ee7563d154ffa38c11bd7351eed
johnnydiabetic <johnnydiabetic@gmail.com>
2014-06-21 15:39:20 -0400

    Added a few changes to the wedding gallery.
     * The gallery now links to the pic
     * /wedding/ links directly to the gallery
     * TODO - thumbnails and pic sizes - looks stinky now

--- wedding/urls.py
+++ wedding/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from wedding import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.root, name='weddingroot'),
+                       url(r'^$', views.gallery, name='weddingroot'),
                        url(r'^gallery/$', views.gallery, name='gallery'),
-                       url(r'^pic/(?P<pic_name>\S+)$', views.pic, name = "view_pic"),
+                       url(r'^pic/(?P<pic_name>\S+)$', views.pic, name="view_pic"),
                        )
