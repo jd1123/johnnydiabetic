355452a0fbde3bd06c55f5657c47fa574fa2dc1f
johnnydiabetic <johnnydiabetic@gmail.com>
2014-06-21 18:26:44 -0400

    Changed the script to load pictures into the DB

--- wedding/populate_pics.py
+++ wedding/populate_pics.py
@@ -1,3 +1,7 @@
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
 from os import listdir
 from os.path import isfile, join
 from wedding.models import WeddingPic
@@ -5,5 +9,9 @@ from wedding.models import WeddingPic
 pic_dir = '/home/jw/protected'
 files = [f for f in listdir(pic_dir) if isfile(join(pic_dir, f))]
 for f in files:
-    print "Adding entry for " + str(f)
-    WeddingPic.objects.create(pic_name=f, caption='')
+    try:
+        p = WeddingPic.objects.get(pic_name=f).pic_name
+        print "Entry already exists for " + str(p)
+    except:
+        print "Adding entry for " + str(f)
+        WeddingPic.objects.create(pic_name=f, caption='')
