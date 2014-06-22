580745b2b3cf11e26735f5710636e7b828c48c94
johnnydiabetic <johnnydiabetic@gmail.com>
2014-06-22 12:22:28 -0400

    Added a few minor changes:
    * Added UserProfile code - but it is still not implemented
    * Updated gallery.css for better formatting in the wedding app
    * Edited the base.html template to enable adding scripts at end of doc
    * Edited gallery.html template to use the new css

--- basefunctions/admin.py
+++ basefunctions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from basefunctions.models import UserProfile
 # Register your models here.
+
+admin.site.register(UserProfile)
--- basefunctions/tests.py
+++ basefunctions/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from basefunctions.models import UserProfile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username = 'nancy',
+                                        password = 'opensesame',
+                                        email = 'nancy@nancy.com')
+        user.save()
+        UserProfile.objects.create(user=user, website="http://www.johnnydiabetic.com")
+
+    def test_creation(self):
+        pfl = UserProfile.objects.all()
+        self.assertFalse(pfl[0]==None, "UserProfile was not successfully created")
