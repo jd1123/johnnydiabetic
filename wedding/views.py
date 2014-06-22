e729acba8028b0ac9ddaeeacd99d9c4fe30dfb7c
johnnydiabetic <johnnydiabetic@gmail.com>
2014-06-21 20:43:09 -0400

    A few formatting changes:
     * Deleted the unnecessary print statements
     * about.html template minor formatting change

--- wedding/views.py
+++ wedding/views.py
@@ -7,30 +7,31 @@ from wedding.models import WeddingPic
 from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
+
 @login_required
 def root(request):
     return HttpResponse('This is the wedding root')
+
 
 @login_required
 def gallery(request):
     context = RequestContext(request)
     context_dict = {}
     wedding_pictures = WeddingPic.objects.all()
-    p = Paginator(wedding_pictures,16)
+    p = Paginator(wedding_pictures, 16)
     context_dict['pics'] = wedding_pictures
     return render_to_response('wedding/gallery.html', context_dict, context)
-    
+
+
 @login_required
 def pic(request, pic_name):
     context = RequestContext(request)
     context_dict = {}
-    print pic_name
     try:
-        pic = WeddingPic.objects.get(pic_name = pic_name)
+        pic = WeddingPic.objects.get(pic_name=pic_name)
         context_dict['pic_name'] = pic.pic_name
         context_dict['caption'] = pic.caption
     except ObjectDoesNotExist:
         return render_to_response('404.html', context_dict, context)
-        
+
     return render_to_response('wedding/pictemplate.html', context_dict, context)
-    #return HttpResponse('Not Yet Implemented')
